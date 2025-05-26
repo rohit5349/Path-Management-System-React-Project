8d3cc34b85164dc68b02c14dcbac1456095a7467
rohit_singh12 <119621966+rohit5349@users.noreply.github.com>
2025-05-26 22:51:56 +0530

    Update chatbot.js

--- api/routes/chatbot.js
+++ api/routes/chatbot.js
@@ -5,38 +5,43 @@ import axios from "axios";
 dotenv.config();
 const router = express.Router();
 
-const OPENAI_API_KEY = process.env.OPEN_API_KEY;
+const API_KEY = process.env.GEMINI_API_KEY;
 
 router.post("/", async (req, res) => {
-    try {
-        const userMessage = req.body.message;
-        if (!userMessage) {
-            return res.status(400).json({ error: "Message is required" });
-        }
+     if(!req.body){
+        return res.status(400).send("No Request body");
+     }
+     console.log("req.body" , req.body);
 
-        const response = await axios.post(
-             "https://api.openai.com/v1/chat/completions",
-            {
-                model: "gpt-3.5-turbo", 
-                messages: [{ role: "user", content: userMessage }],
-                temperature: 0.7
-            },
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${OPENAI_API_KEY}`
-                }
-            }
-        );
+     try{
+        const api_url  =   `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
+        const postData = {
+                 "contents" : [
+                    {
+                       "role": "user",
+                       "parts":[
+                           {
+                               "text" : req.body?.text
+                           }
+                       ]
+                    }
+                 ]
+             
+        };
 
-        const botReply = response.data.choices?.[0]?.message?.content || "I couldn't understand that.";
+        const response = await axios.post(api_url , postData , {
+              headers : {
+                 'Content-Type' : 'application/json'
+              }
+        });
 
-        res.json({ reply: botReply });
-
-    } catch (error) {
-        console.error("Chatbot Error: ", error.response?.data || error.message);
-        res.status(500).json({ error: "Something went wrong!" });
-    }
+        const reply = response.data?.candidates?.[0]?.content?.[0].parts?.[0]?.text || "No responce";
+        res.json({reply});
+       
+     } catch(error) {
+         console.error("Axios Error:", error.response?.data || error.message || error);
+         res.status(500).send("Failed to fetch the response");
+     }
 });
 
 export default router;
